702e175186bd2da32e123656a922f712bb6f5af0
BigRedSped-Laptop <98977703+Nathancecixx@users.noreply.github.com>
2023-10-31 17:35:29 -0400

    Add project files.

---
+++ RockPaperScissors_Test/RockPaperScissors_Test.cpp
@@ -0,0 +1,26 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RockPaperScissorsTest
+{
+	TEST_CLASS(RockPaperScissorsTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+		TEST_METHOD(TestMethod2)
+		{
+		}
+		TEST_METHOD(TestMethod3)
+		{
+		}
+		TEST_METHOD(TestMethod4)
+		{
+		}
+	};
+}
