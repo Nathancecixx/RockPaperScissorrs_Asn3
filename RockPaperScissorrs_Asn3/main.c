a13cd0fdb7a90d7213978c26fb23895bc6ae2148
BigRedSped-Laptop <98977703+Nathancecixx@users.noreply.github.com>
2023-11-03 09:44:25 -0400

    Third test to pass.
    This contains both the player 2 checks and invalid checks as invalid is just the base case, also polished up to submit.

--- RockPaperScissorrs_Asn3/main.c
+++ RockPaperScissorrs_Asn3/main.c
@@ -1,3 +1,9 @@
+//Simple Rock, Paper, Scissors program
+
+//Assignment 3
+//CSCN71020 - fall 23 - Nathan Ceci
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,13 +16,22 @@ void main(void) {
 
 }
 
+
+
+/*************************************************
+*<-----This is the function to check winner----->*
+**************************************************/
+
 char* checkWinner (char* inpOne, char* inpTwo) {
 
+//Create char arrays and copy the array found at 
+//input perameter pointers 
 	char playerOne[MAXCHARS];
 	char playerTwo[MAXCHARS];
 
 	strncpy_s(playerOne, MAXCHARS, inpOne, MAXCHARS - 1);
 	strncpy_s(playerTwo, MAXCHARS, inpTwo, MAXCHARS - 1);
+
 
 	//Allocate memory on the heap to store char array to hold 
 	//the result of the function.
@@ -29,8 +44,6 @@ char* checkWinner (char* inpOne, char* inpTwo) {
 		exit(EXIT_FAILURE);
 	}
 
-	//Copy "Invalid" as base case.
-	strncpy_s(winner, MAXCHARS, "Invalid", MAXCHARS - 1);
 
 	//If both players input same values, its a draw.
 	if (strcmp(playerOne, playerTwo) == 0) {
@@ -38,23 +51,42 @@ char* checkWinner (char* inpOne, char* inpTwo) {
 		return &winner[0];
 	}
 
+
 	//Check if Player1 wins:
-	if (strcmp(playerOne, "Rock") && strcmp(playerTwo, "Scissors")) {
+	if (strcmp(playerOne, "Rock") == 0 && strcmp(playerTwo, "Scissors") == 0) {
 		strncpy_s(winner, MAXCHARS, "Player1", MAXCHARS - 1);
 		return &winner[0];
 	}
 
-	if (strcmp(playerOne, "Paper") && strcmp(playerTwo, "Rock")) {
+	if (strcmp(playerOne, "Paper") == 0 && strcmp(playerTwo, "Rock") == 0) {
 		strncpy_s(winner, MAXCHARS, "Player1", MAXCHARS - 1);
 		return &winner[0];
 	}
 
-	if (strcmp(playerOne, "Scissors") && strcmp(playerTwo, "Paper")) {
+	if (strcmp(playerOne, "Scissors") == 0 && strcmp(playerTwo, "Paper") == 0) {
 		strncpy_s(winner, MAXCHARS, "Player1", MAXCHARS - 1);
 		return &winner[0];
 	}
 
 
+	//Check if Player2 wins:
+	if (strcmp(playerTwo, "Rock") == 0 && strcmp(playerOne, "Scissors") == 0) {
+		strncpy_s(winner, MAXCHARS, "Player2", MAXCHARS - 1);
+		return &winner[0];
+	}
 
+	if (strcmp(playerTwo, "Paper") == 0 && strcmp(playerOne, "Rock") == 0) {
+		strncpy_s(winner, MAXCHARS, "Player2", MAXCHARS - 1);
+		return &winner[0];
+	}
+
+	if (strcmp(playerTwo, "Scissors") == 0 && strcmp(playerOne, "Paper") == 0) {
+		strncpy_s(winner, MAXCHARS, "Player2", MAXCHARS - 1);
+		return &winner[0];
+	}
+
+
+	//Copy "Invalid" as base case if no other conditions are met
+	strncpy_s(winner, MAXCHARS, "Invalid", MAXCHARS - 1);
 	return &winner[0];
 }
--- RockPaperScissors_Test/RockPaperScissors_Test.cpp
+++ RockPaperScissors_Test/RockPaperScissors_Test.cpp
@@ -3,6 +3,8 @@
 
 #include <string.h>
 
+#define MAXCHARS	25
+
 extern "C" char* checkWinner(char*, char*);
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -15,29 +17,37 @@ namespace RockPaperScissorsTest
 
 
 
-//<-----------------Section for testing for draws----------------->
+
+/*******************************************************************
+ *<-----------------Section for testing for draws----------------->*
+ *******************************************************************/
 		TEST_METHOD(TestDraw_One)
 		{
-			//Initialize char arrays
-			char arrOne[25] = { "Rock" };
-			char arrTwo[25] = { "Rock" };
-			char result[25];
+			//Initialize char arrays.
+			char arrOne[MAXCHARS] = { "Rock" };
+			char arrTwo[MAXCHARS] = { "Rock" };
+			char result[MAXCHARS];
+			//Create char pointer to store the return pointer.
+			char* winner = checkWinner(&arrOne[0], &arrTwo[0]);
 
-			//Copy the char array the return pointer points too and store it in result
-			strncpy_s(result, 25, checkWinner(&arrOne[0], &arrTwo[0]), 24);
-
+			//Copy the char array the return pointer points too and store it in result.
+			strncpy_s(result, MAXCHARS, winner, MAXCHARS - 1);
+			//Free the memory allocation to avoid memory leaks.
+			free(winner);
 			//Check weather result is equal to what it should be.
 			Assert::AreEqual("Draw", result);
 		}
 		TEST_METHOD(TestDraw_Two)
 		{
 			//Initialize char arrays
-			char arrOne[25] = { "Paper" };
-			char arrTwo[25] = { "Paper" };
-			char result[25];
+			char arrOne[MAXCHARS] = { "Paper" };
+			char arrTwo[MAXCHARS] = { "Paper" };
+			char result[MAXCHARS];
+			char* winner = checkWinner(&arrOne[0], &arrTwo[0]);
 
 			//Copy the char array the return pointer points too and store it in result
-			strncpy_s(result, 25, checkWinner(&arrOne[0], &arrTwo[0]), 24);
+			strncpy_s(result, MAXCHARS, winner, MAXCHARS - 1);
+			free(winner);
 
 			//Check weather result is equal to what it should be.
 			Assert::AreEqual("Draw", result);
@@ -45,12 +55,14 @@ namespace RockPaperScissorsTest
 		TEST_METHOD(TestDraw_Three)
 		{
 			//Initialize char arrays
-			char arrOne[25] = { "Scissors" };
-			char arrTwo[25] = { "Scissors" };
-			char result[25];
+			char arrOne[MAXCHARS] = { "Scissors" };
+			char arrTwo[MAXCHARS] = { "Scissors" };
+			char result[MAXCHARS];
+			char* winner = checkWinner(&arrOne[0], &arrTwo[0]);
 
 			//Copy the char array the return pointer points too and store it in result
-			strncpy_s(result, 25, checkWinner(&arrOne[0], &arrTwo[0]), 24);
+			strncpy_s(result, MAXCHARS, winner, MAXCHARS - 1);
+			free(winner);
 
 			//Check weather result is equal to what it should be.
 			Assert::AreEqual("Draw", result);
@@ -58,16 +70,21 @@ namespace RockPaperScissorsTest
 
 
 
-//<-----------------Section for testing Player1 wins----------------->
+
+/**********************************************************************
+ *<-----------------Section for testing Player1 wins----------------->*
+ **********************************************************************/
 		TEST_METHOD(TestPlayer1_One)
 		{
 			//Initialize char arrays
-			char arrOne[25] = { "Paper" };
-			char arrTwo[25] = { "Rock" };
-			char result[25];
+			char arrOne[MAXCHARS] = { "Paper" };
+			char arrTwo[MAXCHARS] = { "Rock" };
+			char result[MAXCHARS];
+			char* winner = checkWinner(&arrOne[0], &arrTwo[0]);
 
 			//Copy the char array the return pointer points too and store it in result
-			strncpy_s(result, 25, checkWinner(&arrOne[0], &arrTwo[0]), 24);
+			strncpy_s(result, MAXCHARS, winner, MAXCHARS - 1);
+			free(winner);
 
 			//Check weather result is equal to what it should be.
 			Assert::AreEqual("Player1", result);
@@ -76,12 +93,14 @@ namespace RockPaperScissorsTest
 		TEST_METHOD(TestPlayer1_Two)
 		{
 			//Initialize char arrays
-			char arrOne[25] = { "Rock" };
-			char arrTwo[25] = { "Scissors" };
-			char result[25];
+			char arrOne[MAXCHARS] = { "Rock" };
+			char arrTwo[MAXCHARS] = { "Scissors" };
+			char result[MAXCHARS];
+			char* winner = checkWinner(&arrOne[0], &arrTwo[0]);
 
 			//Copy the char array the return pointer points too and store it in result
-			strncpy_s(result, 25, checkWinner(&arrOne[0], &arrTwo[0]), 24);
+			strncpy_s(result, winner, MAXCHARS - 1);
+			free(winner);
 
 			//Check weather result is equal to what it should be.
 			Assert::AreEqual("Player1", result);
@@ -90,15 +109,125 @@ namespace RockPaperScissorsTest
 		TEST_METHOD(TestPlayer1_Three)
 		{
 			//Initialize char arrays
-			char arrOne[25] = { "Scissors" };
-			char arrTwo[25] = { "Paper" };
-			char result[25];
+			char arrOne[MAXCHARS] = { "Scissors" };
+			char arrTwo[MAXCHARS] = { "Paper" };
+			char result[MAXCHARS];
+			char* winner = checkWinner(&arrOne[0], &arrTwo[0]);
 
 			//Copy the char array the return pointer points too and store it in result
-			strncpy_s(result, 25, checkWinner(&arrOne[0], &arrTwo[0]), 24);
+			strncpy_s(result, winner, MAXCHARS - 1);
+			free(winner);
 
 			//Check weather result is equal to what it should be.
 			Assert::AreEqual("Player1", result);
+		}
+
+
+
+
+/**********************************************************************
+ *<-----------------Section for testing Player2 wins----------------->*
+ **********************************************************************/
+		TEST_METHOD(TestPlayer2_One)
+		{
+			//Initialize char arrays
+			char arrOne[MAXCHARS] = { "Rock" };
+			char arrTwo[MAXCHARS] = { "Paper" };
+			char result[MAXCHARS];
+			char* winner = checkWinner(&arrOne[0], &arrTwo[0]);
+
+			//Copy the char array the return pointer points too and store it in result
+			strncpy_s(result, MAXCHARS, winner, MAXCHARS - 1);
+			free(winner);
+
+			//Check weather result is equal to what it should be.
+			Assert::AreEqual("Player2", result);
+		}
+
+		TEST_METHOD(TestPlayer2_Two)
+		{
+			//Initialize char arrays
+			char arrOne[MAXCHARS] = { "Paper" };
+			char arrTwo[MAXCHARS] = { "Scissors" };
+			char result[MAXCHARS];
+			char* winner = checkWinner(&arrOne[0], &arrTwo[0]);
+
+			//Copy the char array the return pointer points too and store it in result
+			strncpy_s(result, MAXCHARS, winner, MAXCHARS - 1);
+			free(winner);
+
+			//Check weather result is equal to what it should be.
+			Assert::AreEqual("Player2", result);
+		}
+
+		TEST_METHOD(TestPlayer2_Three)
+		{
+			//Initialize char arrays
+			char arrOne[MAXCHARS] = { "Scissors" };
+			char arrTwo[MAXCHARS] = { "Rock" };
+			char result[MAXCHARS];
+			char* winner = checkWinner(&arrOne[0], &arrTwo[0]);
+
+			//Copy the char array the return pointer points too and store it in result
+			strncpy_s(result, winner, MAXCHARS - 1);
+			free(winner);
+
+			//Check weather result is equal to what it should be.
+			Assert::AreEqual("Player2", result);
+		}
+
+
+
+
+/***********************************************************************
+ *<-----------------Section for testing Invalid input----------------->*
+ ***********************************************************************/
+		TEST_METHOD(TestInvalid_One)
+		{
+			//Initialize char arrays
+			char arrOne[MAXCHARS] = { "Steve" };
+			char arrTwo[MAXCHARS] = { "test" };
+			char result[MAXCHARS];
+			char* winner = checkWinner(&arrOne[0], &arrTwo[0]);
+
+			//Copy the char array the return pointer points too and store it in result
+			strncpy_s(result, winner, MAXCHARS - 1);
+			free(winner);
+
+			//Check weather result is equal to what it should be.
+			Assert::AreEqual("Invalid", result);
+		}
+
+		TEST_METHOD(TestInvalid_Two)
+		{
+			//Initialize char arrays
+			char arrOne[MAXCHARS] = { "Paper" };
+			char arrTwo[MAXCHARS] = { "Nathan" };
+			char result[MAXCHARS];
+			char* winner = checkWinner(&arrOne[0], &arrTwo[0]);
+
+			//Copy the char array the return pointer points too and store it in result
+			strncpy_s(result, MAXCHARS, winner, MAXCHARS - 1);
+			free(winner);
+
+			//Check weather result is equal to what it should be.
+			Assert::AreEqual("Invalid", result);
+		}
+
+		TEST_METHOD(TestInvalid_Three)
+		{
+			//Initialize char arrays
+			char arrOne[MAXCHARS] = { "STEVE" };
+			char arrTwo[MAXCHARS] = { "Rock" };
+			char result[MAXCHARS];
+			char* winner = checkWinner(&arrOne[0], &arrTwo[0]);
+
+			//Copy the char array the return pointer points too and store it in result
+			strncpy_s(result, MAXCHARS, winner, MAXCHARS - 1);
+			free(winner);
+
+			//Check weather result is equal to what it should be.
+			Assert::AreEqual("Invalid", result);
 		}
 	};
 }
